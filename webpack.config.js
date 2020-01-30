d4b016da57b91e1a22af41b5e78469d58f568928
Tom Spencer <tom.spencer@studiozeffa.com>
2020-01-30 16:18:56 +0000

    Production settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,12 @@
 const path = require('path');
+const merge = require('webpack-merge');
 
-module.exports = {
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+const common = {
   entry: {
     bundle: './src/index.ts',
   },
@@ -8,13 +14,6 @@ module.exports = {
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'public'),
-  },
-
-  mode: 'development',
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './public',
-    watchContentBase: true,
   },
 
   resolve: {
@@ -28,12 +27,60 @@ module.exports = {
         exclude: /node_modules/,
         use: ['babel-loader'],
       },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+  ],
+};
+
+const development = {
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './public',
+    watchContentBase: true,
+  },
+  module: {
+    rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
     ],
   },
+};
 
-  plugins: [],
+const production = {
+  output: {
+    filename: '[name].[contenthash].js',
+  },
+  devtool: 'source-map',
+  optimization: {
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+      chunkFilename: '[id].[contenthash].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+};
+
+module.exports = (mode = 'development') => {
+  if (mode === 'production') {
+    return merge(common, production, { mode });
+  }
+
+  return merge(common, development, { mode });
 };
